aaadb0a3606c598399cf5fbafbd5aabd8f43eae7
shrektan <shrektan@126.com>
2016-02-24 11:34:25 +0800

    init commit

---
+++ server.R
@@ -0,0 +1,44 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+
+function(input, output, session) {
+#   nowReact <- reactivePoll(intervalMillis = 1000, session, 
+#                            function() Sys.time(),
+#                            function() Sys.time())
+  sessionAutoClose <- reactiveValues(
+    lastInput = Sys.time(), 
+    Now =  reactivePoll(intervalMillis = 1000, session, 
+                        function() Sys.time(),
+                        function() Sys.time())
+  )
+  observe({
+    r <- as.double.difftime(difftime(
+      sessionAutoClose$Now(), sessionAutoClose$lastInput, 
+      units = "secs"), units = "secs")
+    message("check", r) 
+    if (r > 5) {
+      session$close()
+    }
+  })
+  session$onInputReceived({
+    function(...) {message("fire"); sessionAutoClose$lastInput <- Sys.time()}
+  })
+  
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+}
